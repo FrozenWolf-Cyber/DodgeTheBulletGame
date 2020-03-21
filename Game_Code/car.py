fde0bbf006ef4ef0f95a7a276a1a9c7b7bf1da3c
FrozenWolf-Cyber <57902078+FrozenWolf-Cyber@users.noreply.github.com>
2020-03-21 13:13:46 +0530

    Update car.py

--- Game_Code/car.py
+++ Game_Code/car.py
@@ -1,28 +1,28 @@
-
 import pygame
+
 WHITE = (255, 255, 255)
- 
+
+
 class Car(pygame.sprite.Sprite):
-    
-    def __init__(self, color, width, height ,x , y):
+    def __init__(self, color, width, height, x, y):
 
         super().__init__()
 
         self.image = pygame.Surface([width, height])
         self.image.fill(WHITE)
         self.image.set_colorkey(WHITE)
-        self.width=width
-        self.height=height
+        self.width = width
+        self.height = height
         self.color = color
 
         pygame.draw.rect(self.image, self.color, [0, 0, self.width, self.height])
-        
+
         self.rect = self.image.get_rect()
 
         self.rect.x = x
         self.rect.y = y
-        
-    def update(self,keys):
+
+    def update(self, keys):
 
         if keys[pygame.K_LEFT]:
             self.rect.x = self.rect.x - 5
@@ -32,4 +32,3 @@ class Car(pygame.sprite.Sprite):
             self.rect.y = self.rect.y - 5
         if keys[pygame.K_DOWN]:
             self.rect.y = self.rect.y + 5
-
