03982e74fc90de648a29c58f3677e349777f1a92
FrozenWolf-Cyber <57902078+FrozenWolf-Cyber@users.noreply.github.com>
2020-03-21 13:13:20 +0530

    Update bullets.py

--- Game_Code/bullets.py
+++ Game_Code/bullets.py
@@ -3,18 +3,18 @@ import math
 
 WHITE = (255, 255, 255)
 
+
 class bullet(pygame.sprite.Sprite):
-    
-    def __init__(self, color, width, height , x ,y , angle,speed):
+    def __init__(self, color, width, height, x, y, angle, speed):
 
         super().__init__()
 
         self.image = pygame.Surface([width, height])
         self.image.fill(WHITE)
         self.image.set_colorkey(WHITE)
- 
-        pygame.draw.rect(self.image, color,  [0,0,width, height])
-        
+
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -23,12 +23,16 @@ class bullet(pygame.sprite.Sprite):
 
     def update(self):
 
-        self.rect.x = self.rect.x + self.speed*math.cos(math.radians(self.angle))
-        self.rect.y = self.rect.y + self.speed*math.sin(math.radians(self.angle))
-        if self.rect.x >= 800 or self.rect.x<=0 or self.rect.y >=800 or self.rect.y<=0:
+        self.rect.x = self.rect.x + self.speed * math.cos(math.radians(self.angle))
+        self.rect.y = self.rect.y + self.speed * math.sin(math.radians(self.angle))
+        if (
+            self.rect.x >= 800
+            or self.rect.x <= 0
+            or self.rect.y >= 800
+            or self.rect.y <= 0
+        ):
             self.kill()
 
     def destroy(self):
         self.kill()
-
-    
+       
